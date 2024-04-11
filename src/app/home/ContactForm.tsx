33ce1c3860f3e19ac7a89c4aaa4e4123f63f68d6
Joe <jkjoeeekim@live.com>
2024-04-11 15:28:19 -0700

    update form styling

--- src/app/home/ContactForm.tsx
+++ src/app/home/ContactForm.tsx
@@ -8,12 +8,31 @@ const ContactForm = () => {
       <div className='w-1/2'>
         <img src={image.src} className=''></img>
       </div>
-      <div className='form-control text-neutral w-1/2'>
-        <div>
-          <div className='label'>
-            name
+      <div className='w-1/2 flex flex-col items-center'>
+        <div className='form-control w-2/3'>
+          <div className='text-neutral'>
+            <p className='font-extrabold text-4xl text-center'>Don't miss a beat!</p>
+            <p className='font-extrabold text-4xl text-center'>Join our newsletter!</p>
           </div>
-          <input type='text' className='input input-bordered input-warning bg-slate-200'></input>
+          <label className='input input-bordered input-secondary text-warning mt-5 rounded-none bg-slate-200 gap-10 label'>
+            First Name
+            <input type='text' placeholder='John' className='text-neutral grow'></input>
+          </label>
+          <label className='input input-bordered input-secondary text-warning mt-5 rounded-none bg-slate-200 gap-10 label'>
+            Last Name
+            <input type='text' placeholder='Smith' className='text-neutral grow'></input>
+          </label>
+          <label className='input input-bordered input-secondary text-warning mt-5 rounded-none bg-slate-200 gap-20 label'>
+            Email
+            <input type='text' placeholder='example@procademy.com' className='text-neutral grow'></input>
+          </label>
+          <label className="label cursor-pointer justify-start">
+            <input type="checkbox" defaultChecked className="checkbox mt-5 mr-2 checkbox-primary" />
+            <span className="label-text text-warning text-xs">Yes, I would like to receive promotional emails from Procademy</span>
+          </label>
+          <button className='btn btn-primary mt-5'>
+            Submit
+          </button>
         </div>
       </div>
     </div>
