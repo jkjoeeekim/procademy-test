51c607f5589d873986acdfa2d65ff1bc4d1e8f2e
Joe <jkjoeeekim@live.com>
2024-04-11 18:02:49 -0700

    update font and styling

--- src/app/home/Testimonials.tsx
+++ src/app/home/Testimonials.tsx
@@ -11,101 +11,112 @@ import styles from './Home.module.css';
 
 const Testimonials = () => {
   return (
-    <div className="carousel w-full">
-      <div id="slide1" className="carousel-item relative w-full bg-warning">
-        <div className="flex flex-col w-full">
-          <div className='mx-auto text-center m-5 mt-16'>
-            <p className='w-1/2 text-6xl font-extrabold'>
-              TESTIMONIALS
-            </p>
-          </div>
-          <div className='flex flex-row place-content-center m-5 mb-16'>
-            <Image width={320} height={320} alt='Pic' className='mask mask-hexagon w-44 mr-10' src={image1.src}></Image>
-            <div className='w-1/3 flex items-center'>
-              <p className='text-center text-xl'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
-                error amet numquam iure provident voluptate esse quasi, veritatis
-                totam voluptas nostrum quisquam eum porro a pariatur veniam.
+    <div>
+      <div className="carousel w-full">
+        <div id="slide1" className="carousel-item relative w-full bg-warning">
+          <div className="flex flex-col w-full">
+            <div className='mx-auto text-center m-5 mt-16'>
+              <p className='w-1/2 text-6xl font-extrabold'>
+                TESTIMONIALS
               </p>
             </div>
+            <div className='flex flex-row place-content-center m-5 mb-16'>
+              <Image width={320} height={320} alt='Pic' className='mask mask-hexagon w-44 mr-10' src={image1.src}></Image>
+              <div className='w-1/3 flex items-center'>
+                <p className='text-center text-xl'>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
+                  error amet numquam iure provident voluptate esse quasi, veritatis
+                  totam voluptas nostrum quisquam eum porro a pariatur veniam.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" className="btn btn-circle">❮</a>
+            <a href="#slide2" className="btn btn-circle">❯</a>
           </div>
         </div>
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide4" className="btn btn-circle">❮</a>
-          <a href="#slide2" className="btn btn-circle">❯</a>
+        <div id="slide2" className="carousel-item relative w-full bg-warning">
+          <div className="w-full flex flex-col">
+            <div className="flex flex-col w-full">
+            <div className='mx-auto text-center m-5 mt-16'>
+              <p className='w-1/2 text-6xl font-extrabold'>
+                TESTIMONIALS
+              </p>
+            </div>
+            <div className='flex flex-row place-content-center m-5 mb-16'>
+              <div className='w-1/3 flex items-center'>
+                <p className='text-center text-xl'>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
+                  error amet numquam iure provident voluptate esse quasi, veritatis
+                  totam voluptas nostrum quisquam eum porro a pariatur veniam.
+                </p>
+              </div>
+              <Image width={320} height={320} alt='Pic' className='mask mask-hexagon w-44 ml-10' src={image2.src}></Image>
+            </div>
+          </div>
+          </div>
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">❮</a>
+            <a href="#slide3" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+        <div id="slide3" className="carousel-item relative w-full bg-warning">
+          <div className="w-full flex flex-col">
+            <div className="flex flex-col w-full">
+            <div className='mx-auto text-center m-5 mt-16'>
+              <p className='w-1/2 text-6xl font-extrabold'>
+                TESTIMONIALS
+              </p>
+            </div>
+            <div className='flex flex-row place-content-center m-5 mb-16'>
+              <Image width={320} height={320} alt='Pic' className='mask mask-hexagon w-44 mr-10' src={image3.src}></Image>
+              <div className='w-1/3 flex items-center'>
+                <p className='text-center text-xl'>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
+                  error amet numquam iure provident voluptate esse quasi, veritatis
+                  totam voluptas nostrum quisquam eum porro a pariatur veniam.
+                </p>
+              </div>
+            </div>
+          </div>
+          </div>        
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">❮</a>
+            <a href="#slide4" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+        <div id="slide4" className="carousel-item relative w-full bg-warning">
+          <div className="w-full flex flex-col">
+            <div className="flex flex-col w-full">
+            <div className='mx-auto text-center m-5 mt-16'>
+              <p className='w-1/2 text-6xl font-extrabold'>
+                TESTIMONIALS
+              </p>
+            </div>
+            <div className='flex flex-row place-content-center m-5 mb-16'>
+              <div className='w-1/3 flex items-center'>
+                <p className='text-center text-xl'>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
+                  error amet numquam iure provident voluptate esse quasi, veritatis
+                  totam voluptas nostrum quisquam eum porro a pariatur veniam.
+                </p>
+              </div>
+              <Image width={320} height={320} alt='Pic' className='mask mask-hexagon w-44 ml-10' src={image4.src}></Image>
+            </div>
+          </div>
+          </div>
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">❮</a>
+            <a href="#slide1" className="btn btn-circle">❯</a>
+          </div>
         </div>
       </div>
-      <div id="slide2" className="carousel-item relative w-full bg-warning">
-        <div className="w-full flex flex-col">
-          <div className="flex flex-col w-full">
-          <div className='mx-auto text-center m-5 mt-16'>
-            <p className='w-1/2 text-6xl font-extrabold'>
-              TESTIMONIALS
-            </p>
-          </div>
-          <div className='flex flex-row place-content-center m-5 mb-16'>
-            <div className='w-1/3 flex items-center'>
-              <p className='text-center text-xl'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
-                error amet numquam iure provident voluptate esse quasi, veritatis
-                totam voluptas nostrum quisquam eum porro a pariatur veniam.
-              </p>
-            </div>
-            <Image width={320} height={320} alt='Pic' className='mask mask-hexagon w-44 mr-10' src={image2.src}></Image>
-          </div>
-        </div>
-        </div>        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">❮</a>
-          <a href="#slide3" className="btn btn-circle">❯</a>
-        </div>
-      </div>
-      <div id="slide3" className="carousel-item relative w-full bg-warning">
-        <div className="w-full flex flex-col">
-          <div className="flex flex-col w-full">
-          <div className='mx-auto text-center m-5 mt-16'>
-            <p className='w-1/2 text-6xl font-extrabold'>
-              TESTIMONIALS
-            </p>
-          </div>
-          <div className='flex flex-row place-content-center m-5 mb-16'>
-            <Image width={320} height={320} alt='Pic' className='mask mask-hexagon w-44 mr-10' src={image3.src}></Image>
-            <div className='w-1/3 flex items-center'>
-              <p className='text-center text-xl'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
-                error amet numquam iure provident voluptate esse quasi, veritatis
-                totam voluptas nostrum quisquam eum porro a pariatur veniam.
-              </p>
-            </div>
-          </div>
-        </div>
-        </div>        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle">❮</a>
-          <a href="#slide4" className="btn btn-circle">❯</a>
-        </div>
-      </div>
-      <div id="slide4" className="carousel-item relative w-full bg-warning">
-        <div className="w-full flex flex-col">
-          <div className="flex flex-col w-full">
-          <div className='mx-auto text-center m-5 mt-16'>
-            <p className='w-1/2 text-6xl font-extrabold'>
-              TESTIMONIALS
-            </p>
-          </div>
-          <div className='flex flex-row place-content-center m-5 mb-16'>
-            <div className='w-1/3 flex items-center'>
-              <p className='text-center text-xl'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
-                error amet numquam iure provident voluptate esse quasi, veritatis
-                totam voluptas nostrum quisquam eum porro a pariatur veniam.
-              </p>
-            </div>
-            <Image width={320} height={320} alt='Pic' className='mask mask-hexagon w-44 mr-10' src={image4.src}></Image>
-          </div>
-        </div>
-        </div>        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle">❮</a>
-          <a href="#slide1" className="btn btn-circle">❯</a>
-        </div>
+      <div className="flex justify-center w-full py-2 gap-2">
+        <a href="#slide1" className="btn btn-m">1</a>
+        <a href="#slide2" className="btn btn-m">2</a>
+        <a href="#slide3" className="btn btn-m">3</a>
+        <a href="#slide4" className="btn btn-m">4</a>
       </div>
     </div>
   )
