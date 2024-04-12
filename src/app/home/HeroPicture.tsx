9bed4ccde1c2425b757dca68e674e7ef4b4ff47f
Joe <jkjoeeekim@live.com>
2024-04-11 18:47:29 -0700

    add responsiveness to hero picture

--- src/app/home/HeroPicture.tsx
+++ src/app/home/HeroPicture.tsx
@@ -14,36 +14,41 @@ import image10 from '../assets/pic3.jpg'
 
 import styles from './Home.module.css'
 
+let topLayerClassName = 'mask mask-hexagon sm:w-32 sm:m-1 md:w-48 md:m-2 lg:w-64 lg:m-4';
+let otherLayerClassName = 'mask mask-hexagon sm:w-32 sm:m-1 sm:-mt-16 md:w-48 md:m-2 md:-mt-20 lg:w-64 lg:-mt-24 lg:m-4';
+let topLayerEmptyDiv = 'mask mask-hexagon sm:w-32 sm:m-1 md:w-48 md:m-2 lg:w-64 lg:m-4';
+let otherLayerEmptyDiv = 'mask mask-hexagon sm:w-32 sm:m-1 md:w-48 md:m-2 lg:w-64 lg:m-4';
+
 const HeroPicture = () => {
   return (
     <div className='flex flex-col'>
       <div className='flex flex-row place-content-center'>
-        <div className='mask mask-hexagon w-64 m-4'></div>
-        <Image width={320} height={320} alt='Pic' className='mask mask-hexagon w-64 m-4' src={image7.src}></Image>
-        <div className='mask mask-hexagon w-64 m-4'></div>
-        <div className='mask mask-hexagon w-64 m-4'></div>
+        <div className={topLayerEmptyDiv}></div>
+        <Image width={320} height={320} alt='Pic' className={topLayerClassName} src={image7.src}></Image>
+        <div className={topLayerEmptyDiv}></div>
+        <div className={topLayerEmptyDiv}></div>
       </div>
       <div className='flex flex-row place-content-center'>
-        <Image width={320} height={320} alt='Pic' className='mask mask-hexagon w-64 -mt-24 m-4' src={image1.src}></Image>
-        <Image width={320} height={320} alt='Pic' className='mask mask-hexagon w-64 -mt-24 m-4' src={image2.src}></Image>
-        <div className='mask mask-hexagon w-64 m-4'></div>
+        <Image width={320} height={320} alt='Pic' className={otherLayerClassName} src={image1.src}></Image>
+        <Image width={320} height={320} alt='Pic' className={otherLayerClassName} src={image2.src}></Image>
+        <div className={otherLayerEmptyDiv}></div>
       </div>
       <div className='flex flex-row place-content-center'>
-        <Image width={320} height={320} alt='Pic' className='mask mask-hexagon w-64 -mt-24 m-4' src={image3.src}></Image>
-        <Image width={320} height={320} alt='Pic' className='mask mask-hexagon w-64 -mt-24 m-4' src={image4.src}></Image>
-        <Image width={320} height={320} alt='Pic' className='mask mask-hexagon w-64 -mt-24 m-4' src={image5.src}></Image>
-        <Image width={320} height={320} alt='Pic' className='mask mask-hexagon w-64 -mt-24 m-4' src={image6.src}></Image>
+        <Image width={320} height={320} alt='Pic' className={otherLayerClassName} src={image3.src}></Image>
+        <Image width={320} height={320} alt='Pic' className={otherLayerClassName} src={image4.src}></Image>
+        <Image width={320} height={320} alt='Pic' className={otherLayerClassName} src={image5.src}></Image>
+        <Image width={320} height={320} alt='Pic' className={otherLayerClassName} src={image6.src}></Image>
       </div>
       <div className='flex flex-row place-content-center'>
-        <div className='mask mask-hexagon w-64 m-4'></div>
-        <Image width={320} height={320} alt='Pic' className='mask mask-hexagon w-64 -mt-24 m-4' src={image8.src}></Image>
-        <Image width={320} height={320} alt='Pic' className='mask mask-hexagon w-64 -mt-24 m-4' src={image9.src}></Image>
+        <div className={otherLayerEmptyDiv}></div>
+        <Image width={320} height={320} alt='Pic' className={otherLayerClassName} src={image8.src}></Image>
+        <Image width={320} height={320} alt='Pic' className={otherLayerClassName} src={image9.src}></Image>
       </div>
       <div className='flex flex-row place-content-center'>
-        <div className='mask mask-hexagon w-64 m-4'></div>
-        <div className='mask mask-hexagon w-64 m-4'></div>
-        <Image width={320} height={320} alt='Pic' className='mask mask-hexagon w-64 -mt-24 m-4' src={image10.src}></Image>
-        <div className='mask mask-hexagon w-64 m-4'></div>
+        <div className={otherLayerEmptyDiv}></div>
+        <div className={otherLayerEmptyDiv}></div>
+        <Image width={320} height={320} alt='Pic' className={otherLayerClassName} src={image10.src}></Image>
+        <div className={otherLayerEmptyDiv}></div>
       </div>
     </div>
     // <div className='flex flex-col'>
